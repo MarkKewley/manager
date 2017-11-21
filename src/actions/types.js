6747d1e862f0d34c52b6e0357b40cf3f25a564ee
Kewley, Mark <mark.kewley@windlogics.com>
2017-11-20 19:12:36 -0600

    EmployeeCreate form functional

--- src/actions/types.js
+++ src/actions/types.js
@@ -1,5 +1,9 @@
+// Login actions
 export const EMAIL_CHANGED = 'email_changed';
 export const PASSWORD_CHANGED = 'password_changed';
 export const LOGIN_USER = 'login_user';
 export const LOGIN_USER_SUCCESS = 'login_user_success';
 export const LOGIN_USER_FAILED = 'login_user_failed';
+
+// Employee updating actions
+export const EMPLOYEE_UPDATE = 'employee_update';
--- src/components/EmployeeCreate.js
+++ src/components/EmployeeCreate.js
@@ -1,37 +1,69 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
-import { Card, CardSection, Input, Button } from './common';
+import { Picker, Text } from 'react-native';
+import { connect } from 'react-redux';
+import { employeeUpdate } from '../actions';
+import { Button, Card, CardSection, Input } from './common';
 
 class EmployeeCreate extends Component {
+
   render () {
     return (
       <Card>
         <CardSection>
           <Input
+            value={this.props.name}
             label='Name'
             placeholder='Jane'
+            onChangeText={value => this.props.employeeUpdate({prop: 'name', value})}
           />
         </CardSection>
 
         <CardSection>
           <Input
+            value={this.props.phone}
             label='Phone'
             placeholder='555-555-555'
+            onChangeText={value => this.props.employeeUpdate({prop: 'phone', value})}
           />
         </CardSection>
 
-        <CardSection>
-
+        <CardSection style={{flexDirection: 'column'}}>
+          <Text style={styles.pickerLabelStyle}>Shift</Text>
+          <Picker
+            selectedValue={this.props.shift}
+            onValueChange={value => this.props.employeeUpdate({prop: 'shift', value})}
+          >
+            <Picker.Item label='Monday' value='Monday'/>
+            <Picker.Item label='Tuesday' value='Tuesday'/>
+            <Picker.Item label='Wednesday' value='Wednesday'/>
+            <Picker.Item label='Thursday' value='Thursday'/>
+            <Picker.Item label='Friday' value='Friday'/>
+            <Picker.Item label='Saturday' value='Saturday'/>
+            <Picker.Item label='Sunday' value='Sunday'/>
+          </Picker>
         </CardSection>
 
         <CardSection>
-            <Button>
-              Create
-            </Button>
+          <Button>
+            Create
+          </Button>
         </CardSection>
       </Card>
     );
   }
+
 }
 
-export default EmployeeCreate;
+const styles = {
+  pickerLabelStyle: {
+    fontSize: 18,
+    paddingLeft: 20
+  }
+};
+
+const mapStateToProps = state => {
+  const {name, phone, shift} = state.employeeForm;
+  return {name, phone, shift};
+};
+
+export default connect(mapStateToProps, {employeeUpdate})(EmployeeCreate);
