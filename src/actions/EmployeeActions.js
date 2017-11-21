a1f208e0d4164c5639a757011e952a23f06c7cf5
Kewley, Mark <mark.kewley@windlogics.com>
2017-11-21 15:02:40 -0600

    firebase with database access, creating employees now

--- src/actions/EmployeeActions.js
+++ src/actions/EmployeeActions.js
@@ -1,8 +1,25 @@
-import { EMPLOYEE_UPDATE } from './types';
+import firebase from 'firebase';
+import { Actions } from 'react-native-router-flux';
+import { EMPLOYEE_CREATE, EMPLOYEE_UPDATE } from './types';
 
 export const employeeUpdate = ({prop, value}) => {
   return {
     type: EMPLOYEE_UPDATE,
     payload: {prop, value}
-  }
+  };
+};
+
+export const employeeCreate = ({name, phone, shift}) => {
+  const {currentUser} = firebase.auth();
+
+  return async dispatch => {
+    // get access to our db and get a reference to the path to the JSON data store provided
+    await firebase.database().ref(`/users/${currentUser.uid}/employees`)
+      .push({name, phone, shift});
+
+    dispatch({type: EMPLOYEE_CREATE});
+
+    // go back one on the stack (will not show back arrow)
+    Actions.pop();
+  };
 };
--- src/reducers/EmployeeFormReducer.js
+++ src/reducers/EmployeeFormReducer.js
@@ -1,17 +1,20 @@
 import {
+  EMPLOYEE_CREATE,
   EMPLOYEE_UPDATE
 } from '../actions/types';
 
 const INITIAL_STATE = {
   name: '',
   phone: '',
-  shift: 'Monday'
+  shift: ''
 };
 
-export default (state = INITIAL_STATE, action)  => {
-  switch(action.type) {
+export default (state = INITIAL_STATE, action) => {
+  switch (action.type) {
     case EMPLOYEE_UPDATE:
-      return { ...state, [action.payload.prop]: action.payload.value };
+      return {...state, [action.payload.prop]: action.payload.value};
+    case EMPLOYEE_CREATE:
+      return INITIAL_STATE;
     default:
       return state;
   }
