2b2fc716712b1dea6e8e8c7ae55dfb2cb0888561
Kewley, Mark <mark.kewley@windlogics.com>
2017-11-22 13:18:20 -0600

    Rendering list of employees:

--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -1,24 +1,57 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { ListView } from 'react-native';
 import { connect } from 'react-redux';
+import _ from 'lodash';
 import { employeesFetch } from '../actions/EmployeeActions';
 import { Spinner } from './common/Spinner';
+import EmployeeListItem from './EmployeeListItem';
 
 class EmployeeList extends Component {
 
   componentWillMount () {
     this.props.employeesFetch();
+    this.createDataSource(this.props);
+  }
+
+  componentWillReceiveProps (nextProps) {
+    // nextProps are the next set of props that this component will be rendered with
+    // this.props is still the OLD set of props
+    this.createDataSource(nextProps);
+  }
+
+  createDataSource ({employees}) {
+    const ds = new ListView.DataSource({
+      rowHasChanged: (r1, r2) => r1 !== r2
+    });
+
+    this.dataSource = ds.cloneWithRows(employees);
+  }
+
+  renderRow(employee) {
+    return <EmployeeListItem employee={employee} />;
   }
 
   render () {
-    return <Spinner/>;
+    if (this.props.loading) {
+      return <Spinner/>;
+    }
+
+    return (
+      <ListView
+        enableEmptySections
+        dataSource={this.dataSource}
+        renderRow={employee => this.renderRow(employee)}
+      />
+    )
   }
 
 }
 
 const mapStateToProps = state => {
-  const {employees} = state;
-  return {employees};
+  const employees = _.map(state.employees.employees, (val, uid) => {
+    return {...val, uid}; // { shift: 'Monday, name: 'Sue', uuid: '1234', ... }
+  });
+  return {employees, loading: state.employees.loading};
 };
 
 export default connect(mapStateToProps, {employeesFetch})(EmployeeList);
--- src/reducers/EmployeeReducer.js
+++ src/reducers/EmployeeReducer.js
@@ -1,11 +1,14 @@
 import { EMPLOYEES_FETCH_SUCCESS } from '../actions/types';
 
-const INITIAL_STATE = {};
+const INITIAL_STATE = {
+  loading: true,
+  employees: {}
+};
 
 export default (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case EMPLOYEES_FETCH_SUCCESS:
-      return action.payload;
+      return { ...state, loading: false, employees: action.payload };
     default:
       return state;
   }
